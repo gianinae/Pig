e47a9e06cb729ccc89e28481f6f48d434b15182f
Elias <Elias@GIANINA>
2017-02-07 17:30:16 -0600

    LISTO JUGADOR

--- PIG/PIG/Form1.Designer.vb
+++ PIG/PIG/Form1.Designer.vb
@@ -22,9 +22,130 @@ Partial Class Form1
     'No lo modifique con el editor de código.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.BtnTirar = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.TxtJugador = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TxtDado = New System.Windows.Forms.TextBox()
+        Me.TxtAcumula = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'BtnTirar
+        '
+        Me.BtnTirar.Location = New System.Drawing.Point(26, 170)
+        Me.BtnTirar.Name = "BtnTirar"
+        Me.BtnTirar.Size = New System.Drawing.Size(75, 23)
+        Me.BtnTirar.TabIndex = 0
+        Me.BtnTirar.Text = "Tirar"
+        Me.BtnTirar.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(26, 200)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 1
+        Me.Button2.Text = "Parar"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'TxtJugador
+        '
+        Me.TxtJugador.Location = New System.Drawing.Point(12, 24)
+        Me.TxtJugador.Name = "TxtJugador"
+        Me.TxtJugador.Size = New System.Drawing.Size(100, 20)
+        Me.TxtJugador.TabIndex = 2
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(172, 24)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox2.TabIndex = 3
+        '
+        'TxtDado
+        '
+        Me.TxtDado.Location = New System.Drawing.Point(92, 74)
+        Me.TxtDado.Name = "TxtDado"
+        Me.TxtDado.Size = New System.Drawing.Size(100, 20)
+        Me.TxtDado.TabIndex = 4
+        '
+        'TxtAcumula
+        '
+        Me.TxtAcumula.Location = New System.Drawing.Point(92, 119)
+        Me.TxtAcumula.Name = "TxtAcumula"
+        Me.TxtAcumula.Size = New System.Drawing.Size(100, 20)
+        Me.TxtAcumula.TabIndex = 5
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(13, 5)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(39, 13)
+        Me.Label1.TabIndex = 6
+        Me.Label1.Text = "Label1"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(172, 4)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(39, 13)
+        Me.Label2.TabIndex = 7
+        Me.Label2.Text = "Label2"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(119, 58)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(39, 13)
+        Me.Label3.TabIndex = 8
+        Me.Label3.Text = "Label3"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(122, 101)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(39, 13)
+        Me.Label4.TabIndex = 9
+        Me.Label4.Text = "Label4"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 261)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TxtAcumula)
+        Me.Controls.Add(Me.TxtDado)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TxtJugador)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.BtnTirar)
+        Me.Name = "Form1"
         Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
 
+    Friend WithEvents BtnTirar As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents TxtJugador As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TxtDado As TextBox
+    Friend WithEvents TxtAcumula As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
 End Class
--- PIG/PIG/Form1.vb
+++ PIG/PIG/Form1.vb
@@ -1,3 +1,40 @@
 ﻿Public Class Form1
+    Dim dado As Integer = 0
+    Dim acumGlobal As Integer = 0
+    Dim acum1 As Integer = 0
+    Dim jugador As Integer = 0
+
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles BtnTirar.Click
+        If jugador = 0 Then
+            dado = random()
+            TxtDado.Text = dado
+            If dado = 1 Then
+                acumGlobal = 0
+            End If
+            acumGlobal = acumGlobal + dado
+            TxtAcumula.Text = acumGlobal
+        End If
+
+
+    End Sub
+
+
+    Function random()
+        Randomize()
+        Dim Num As Integer = CInt(Int((6 * Rnd()) + 1))
+        Return Num
+
+    End Function
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+
+        acum1 = acumGlobal + acum1
+        acumGlobal = 0
+        TxtJugador.Text = acum1
+        jugador = 1
+
+
+    End Sub
 
 End Class
